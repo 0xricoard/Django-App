14d123b4f9b7f1132ab551f0e18e4ddfae20c55d
Muhammad Rico Ardiansyah <115982526+0xricoard@users.noreply.github.com>
2023-10-07 19:38:37 +0700

    Update mobil_form.py

--- django_app/forms/mobil_form.py
+++ django_app/forms/mobil_form.py
@@ -27,24 +27,9 @@ class MobilForm(forms.Form):
     ]
 
     year = forms.ChoiceField(choices=YEAR_CHOICES)
-    mileage = forms.CharField()  # Gunakan CharField untuk memastikan input adalah string
-    tax = forms.CharField()
-    mpg = forms.CharField()
-    engineSize = forms.CharField()
+    mileage = forms.IntegerField()
+    tax = forms.IntegerField()
+    mpg = forms.FloatField()
+    engineSize = forms.FloatField()
     transmission = forms.ChoiceField(choices=TRANSMISSION_CHOICES)
     fuelType = forms.ChoiceField(choices=FUEL_TYPE_CHOICES)
-
-    def clean(self):
-        cleaned_data = super().clean()
-
-        # Ubah variabel-variabel tertentu ke tipe data float
-        try:
-            cleaned_data['mileage'] = float(cleaned_data['mileage'])
-            cleaned_data['tax'] = float(cleaned_data['tax'])
-            cleaned_data['mpg'] = float(cleaned_data['mpg'])
-            cleaned_data['engineSize'] = float(cleaned_data['engineSize'])
-        except ValueError:
-            # Tangani kesalahan jika nilai tidak dapat diubah menjadi float
-            raise forms.ValidationError("Masukkan angka yang valid.")
-
-        return cleaned_data
